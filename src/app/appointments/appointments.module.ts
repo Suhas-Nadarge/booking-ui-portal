8e95ed715050463902162ef3cceea81fc3c6e9d1
Suhas Nadarge <suhas.nadarge@mnx.com>
2022-03-05 03:06:35 +0000

    style changes, added view appointment table, history component and routing for all header

--- src/app/appointments/appointments.module.ts
+++ src/app/appointments/appointments.module.ts
@@ -12,10 +12,11 @@ import {
 import { CommonModule } from '@angular/common';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgxAutocompleteModule } from 'ngx-angular-autocomplete';
+import { PatientHistoryComponent } from './patient-history/patient-history.component';
  
 
 @NgModule({
-  declarations: [BookAppointmentComponent,SearchDoctorComponent,ViewAppointmentsComponent],
+  declarations: [BookAppointmentComponent,SearchDoctorComponent,ViewAppointmentsComponent, PatientHistoryComponent],
   imports: [CommonModule,
     AppointmentsRoutingModule,
     IgxCalendarModule,
